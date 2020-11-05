8397bc155b612eba2af9e10f25308e42dfc14278
“Hetal <hetal_patel46@yahoo.com.com>
2020-11-05 14:11:52 +0530

    Initial commit

---
+++ PracticalTest/PracticalTest/PTListController.swift
@@ -0,0 +1,20 @@
+//
+//  PTListController.swift
+//  PracticalTest
+//
+//  Created by Hetal Patel on 05/11/20.
+//  Copyright © 2020 Hetal Patel. All rights reserved.
+//
+
+import UIKit
+
+class PTListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
